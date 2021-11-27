0386a56ee10ee05b65e3647526113ed2048e8fcd
Filip <f3kula@gmail.com>
2021-11-27 17:54:42 +0100

    HTML subpage
    6 of 12 categories are ready (czym jest html, komentarze, naglowek i paragraf, formatowanie, listy, klasy i identyfikatory), added script to that heades to main page after click on the banner, some stlying changes

---
+++ assets/scripts/main.js
@@ -0,0 +1,5 @@
+const HEADER = document.getElementById('header');
+HEADER.addEventListener('click', () => {
+    window.location.href="../index.html";
+})
+const LIST_ELEMENT = document.getElementsByClassName('list-group-item');

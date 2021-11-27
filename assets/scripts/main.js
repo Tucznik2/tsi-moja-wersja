2716ce8381bd0009f91acb3f4fca83b75b50b4a4
Filip <f3kula@gmail.com>
2021-11-27 23:05:13 +0100

    appearence fixes
    evenet lister for position of side nav to position it, color of active nav element to orange, styled back to top button, position of side nav to sticky, added back to top button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,15 @@
 const HEADER = document.getElementById('header');
+const SIDE_NAV = document.getElementById('side-nav');
+
+let sideNavTopPos;
+
 HEADER.addEventListener('click', () => {
     window.location.href="../index.html";
 })
-const LIST_ELEMENT = document.getElementsByClassName('list-group-item');
+
+window.addEventListener('scroll', () => {
+    sideNavTopPos = SIDE_NAV.offsetTop;
+    if(sideNavTopPos > 550){
+        SIDE_NAV.style.top = 50 + 'px';
+    }
+})
